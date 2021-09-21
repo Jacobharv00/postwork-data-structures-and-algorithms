342f091c337cba874e3753a0d7cd15cd358f59f3
jacob <jacobharv00@gmail.com>
2021-09-21 15:32:20 -0600

    finished fibonacci

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,24 +1,40 @@
-function fibonacci(num) {
-  // type your code here
+// Using terany operator
+
+function fibonacci ( n ) {
+  return n < 1 ? 0
+    : n <= 2 ? 1
+      : fibonacci( n - 1 ) + fibonacci( n - 2 )
 }
 
-if (require.main === module) {
+
+// Using if else statement
+
+// function fibonacci ( num ) {
+//   if ( num <= 1 ) {
+//     return num
+//   } else {
+//     return fibonacci( num - 1 ) + fibonacci( num - 2 )
+//   }
+// }
+
+
+
+if ( require.main === module ) {
   // add your own tests in here
-  console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+  console.log( "Expecting: 0" )
+  console.log( "=>", fibonacci( 0 ) )
 
-  console.log("");
+  console.log( "" )
 
-  console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+  console.log( "Expecting: 1" )
+  console.log( "=>", fibonacci( 2 ) )
 
-  console.log("");
+  console.log( "" )
 
-  console.log("Expecting: 55");
-  console.log("=>", fibonacci(10));
+  console.log( "Expecting: 55" )
+  console.log( "=>", fibonacci( 10 ) )
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+
