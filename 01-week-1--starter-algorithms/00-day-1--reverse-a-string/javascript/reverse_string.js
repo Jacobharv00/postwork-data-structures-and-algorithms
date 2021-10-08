638d189b0c31c5977a7bdb5a6eb0565505fc56ec
jacob <jacobharv00@gmail.com>
2021-10-08 09:53:19 -0600

    finished reverseString

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,34 +1,33 @@
-function reverseString(str) {
+function reverseString ( str ) {
   let reversed = ''
-  
+
   for ( const letter of str ) {
     reversed = letter + reversed
   }
   return reversed
 }
 
+// need to look at every character in the string and add it back in 
+// need to make it so when a argument of a string is givin it takes that string & reverses it.
 
-if (require.main === module) {
+if ( require.main === module ) {
   // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log( "Expecting: 'ih'" )
+  console.log( "=>", reverseString( "hi" ) )
 
-  console.log("");
+  console.log( "" )
 
-  console.log("Expecting: 'ybabtac'");
-  console.log( "=>", reverseString( "catbaby" ) );
-  
-  console.log('Without function call', 'Jacob' )
+  console.log( "Expecting: 'ybabtac'" )
+  console.log( "=>", reverseString( "catbaby" ) )
+
+  console.log( 'Without function call', 'Jacob' )
   console.log( 'With function call', reverseString( 'Jacob' ) )
-  
+
   console.log( 'Without function call', 'Natasha' )
   console.log( 'With function call', reverseString( 'Natasha' ) )
 
 
 }
 
-module.exports = reverseString;
+module.exports = reverseString
 
-// need to look at every character in the string and add it back in 
-
-// need to make it so when a argument of a string is givin it takes that string & reverses it.
