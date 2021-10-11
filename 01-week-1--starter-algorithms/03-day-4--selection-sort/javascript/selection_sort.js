c5608efeea1f6b96123ef0e06fdfb1e57044427f
jacob <jacobharv00@gmail.com>
2021-10-11 11:31:33 -0600

    completed selection sort javascript

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,5 +1,18 @@
-function selectionSort(arr) {
-  // type your code here
+// function selectionSort ( arr ) {
+//   let ordered = arr.sort( ( a, b ) => a - b )
+//   return ordered
+// }
+
+function selectionSort ( arr ) {
+  const sortedArr = []
+
+  while ( arr.length > 0 ) {
+    let min = Math.max( ...arr ) //gives us lowest number in array
+    let index = arr.indexOf( min ) // find the index of minimum 
+    sortedArr.unshift( min ) // push minimum number into new array
+    arr.splice( index, 1 ) // remove that number and continue thru loop
+  }
+  return sortedArr
 }
 
 if (require.main === module) {
@@ -21,3 +34,15 @@ module.exports = selectionSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+// Input: [ 3, -1, 5, 2 ]
+// Output: [ -1, 2, 3, 5 ]
+
+// Sort an Array of integers using selection sort.
+// Look for the lowest value integer and place it in correct order (Sorted)
+// Loop through and check one element to next element and see if it is greater and order it by that. 
+// Main thing we want to do is compare two numbers together and see if left is greater than right and if so move right 1. 
+// If the result is negative then A is sorted before B
+//  If it is positive then B is sorted before A
+//  If 0 then it does nothing and just returns. (Already sorted)
