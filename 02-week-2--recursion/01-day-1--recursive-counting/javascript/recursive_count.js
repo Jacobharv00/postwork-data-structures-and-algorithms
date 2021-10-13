13e8393506bea8ff7454a0c0c9ba41ab8eeea22f
jacob <jacobharv00@gmail.com>
2021-10-13 17:08:42 -0600

    completed recursiveCount

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,13 +1,37 @@
-function recursiveCount(num = 0) {
-  // type your code here
+function recursiveCount ( num = 0 ) {
+  if ( num >= 10 ) return
+
+  console.log( num )
+  console.log( num + 1 )
+  console.log( num + 2 )
+  console.log( num + 3 )
+  console.log( num + 4 )
+
+
+  recursiveCount( ++num )
+
 }
 
-if (require.main === module) {
-  recursiveCount();
+
+if ( require.main === module ) {
+  recursiveCount()
 }
 
-module.exports = recursiveCount;
+module.exports = recursiveCount
 
-// OPTIONAL
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+
+
+
+
+//  sudo
+
+// convert while loop to a recursive function that counts up by one.
+// recursive function success if it prints numbers 0-9
+
+// I need to set a base case if num >= 10 return 'done'
+// call function inside itself that increments num + 1 each call till it reaches base case.
+
+
+
+
+
