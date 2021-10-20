1baf2465988ec0f6ee808b2ae7f2195b02171292
jacob <jacobharv00@gmail.com>
2021-10-20 11:57:12 -0600

    completed recursive shortest string

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
@@ -1,24 +1,68 @@
-function findShortestStringRecursive(arr) {
-  // type your code here
+function findShortestStringRecursive ( arr ) {
+  // LINES 3-12 CLEANER SOLUTION
+  //if ( arr.length === 1 ) return arr[ 0 ]
+  
+  // let first = arr[ 0 ]
+  // let second = findShortestStringRecursive( arr.slice( 1 ) ) // slice makes copy non destructive
+  
+  // if ( first.length <= second.length ) {
+  //     return first
+  //   } else {
+  //     return second
+  // }
+  
+  // OUR SOLUTION
+  let first = arr[ 0 ]
+  let second = arr[ 1 ]
+  
+  while ( arr.length > 1 ) {
+    if ( first.length <= second.length ) {
+      arr.splice( 1, 1 )
+      return findShortestStringRecursive( arr )
+    } else {
+      arr.splice( 0, 1 )
+      return findShortestStringRecursive( arr )
+    }
+  }
+  return first
 }
+console.log( findShortestStringRecursive( [ 'aaa', 'a', 'bb', 'ccc' ] ) )
+
+
+
+
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
   console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
-
+  
   console.log("");
-
+  
   console.log("Expecting: 'hi'");
   console.log("=>", findShortestStringRecursive(['cat', 'hi', 'dog', 'an']));
-
+  
   console.log("");
-
+  
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
 }
 
 module.exports = findShortestStringRecursive;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// input is going to be an Array of strings.
+// return the shortest string
+// If there is more then one string with that same length return the first one.
+// Array will have minimum length of 1
+// wont be an empty string???
+
+// Examples
+// Input: ['aaa', 'a', 'bb', 'ccc']
+// Output: 'a'
+
+// Input: ['cat', 'hi', 'dog', 'an']
+// Output: 'hi'
+
+// Input: ['flower', 'juniper', 'lily', 'dandelion']
+// Output: 'lily'
+
