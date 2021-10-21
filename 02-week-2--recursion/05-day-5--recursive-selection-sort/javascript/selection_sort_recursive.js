ef074c44afecc8de2e994d7f13f8111438f63b6e
jacob <jacobharv00@gmail.com>
2021-10-21 11:26:43 -0600

    completed selection sort recursive

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,6 +1,19 @@
-function selectionSortRecursive(arr) {
-  // type your code here
+function selectionSortRecursive ( arr ) {
+  if ( arr.length === 0 ) return []
+  
+  const min = Math.min( ...arr )
+  const idx = arr.indexOf( min )
+  arr.splice( idx, 1 )
+
+  const result = selectionSortRecursive( arr )
+
+  result.unshift( min )
+  
+  return result
 }
+
+selectionSortRecursive( [ 3, -1, 5, 2 ] )
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -12,5 +25,12 @@ if (require.main === module) {
 
 module.exports = selectionSortRecursive;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+
+
+// SUDO BRUH
+// an array is our input
+// base case is if array is equal to 1
+// every time our recursive function is called we need to check one element to the next and whichever is smaller push into new array.
+// our array will contain integers
+
+
